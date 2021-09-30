22d05a154732e06eda0b1736996b03d4242afb66
a <a@example.com>
2021-10-01 02:10:30 +0900

    add cache to gmo fetcher

--- crypto_data_fetcher/gmo.py
+++ crypto_data_fetcher/gmo.py
@@ -5,38 +5,57 @@ import datetime
 import urllib.request
 from .utils import create_null_logger
 
+def url_exists(url):
+    try:
+        time.sleep(1)
+        res = urllib.request.urlopen(url)
+        if res.getcode() == 200:
+            return True
+    except urllib.error.HTTPError:
+        pass
+    return False
+
+def url_read_csv(url):
+    try:
+        time.sleep(1)
+        df = pd.read_csv(url)
+        df = df.rename(columns={
+            'symbol': 'market',
+        })
+        df['price'] = df['price'].astype('float64')
+        df['size'] = df['size'].astype('float64')
+        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
+        df['side'] = np.where(df['side'] == 'BUY', 1, -1).astype('int8')
+        return df
+    except urllib.error.HTTPError:
+        pass
+    return None
+
 class GmoFetcher:
-    def __init__(self, logger=create_null_logger(), ccxt_client=None):
+    def __init__(self, logger=create_null_logger(), ccxt_client=None, memory=None):
         self.logger = logger
         self.ccxt_client = ccxt_client
 
-    def fetch_trades(self, market=None):
+        if memory is None:
+            self._url_exists = url_exists
+            self._url_read_csv = url_read_csv
+        else:
+            url_exists_cached = memory.cache(url_exists)
+            self._url_exists = url_exists_cached
+
+            url_read_csv_cached = memory.cache(url_read_csv)
+            self._url_read_csv = url_read_csv_cached
+
+    def fetch_ohlcv(self, interval_sec=None, market=None):
+        return self.fetch_trades(market=market, interval_sec=interval_sec)
+
+    def fetch_trades(self, market=None, interval_sec=None):
+        if interval_sec is not None:
+            if 3600 % interval_sec != 0:
+                raise Exception('3600 % interval_sec must be 0')
+
         today = datetime.datetime.now().date()
-
-        for year in range(2018, today.year + 1):
-            url = 'https://api.coin.z.com/data/trades/{}/{}/'.format(market, year)
-            time.sleep(1)
-            self.logger.debug(url)
-            try:
-                res = urllib.request.urlopen(url)
-                if res.getcode() == 200:
-                    start_year = year
-                    break
-            except urllib.error.HTTPError:
-                pass
-
-        for month in range(1, 13):
-            url = 'https://api.coin.z.com/data/trades/{}/{}/{:02}/'.format(market, start_year, month)
-            time.sleep(1)
-            self.logger.debug(url)
-            try:
-                res = urllib.request.urlopen(url)
-                if res.getcode() == 200:
-                    start_month = month
-                    break
-            except urllib.error.HTTPError:
-                pass
-
+        start_year, start_month = self._find_start_year_month(market)
         date = datetime.date(start_year, start_month, 1)
 
         dfs = []
@@ -50,25 +69,50 @@ class GmoFetcher:
                 date.day,
                 market,
             )
-            time.sleep(1)
             self.logger.debug(url)
-            try:
-                df = pd.read_csv(url)
-                df = df.rename(columns={
-                    'symbol': 'market',
-                })
-                df['price'] = df['price'].astype('float64')
-                df['size'] = df['size'].astype('float64')
-                df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
-                df['side'] = np.where(df['side'] == 'BUY', 1, -1).astype('int8')
+            df = self._url_read_csv(url)
+
+            if df is not None:
+                if interval_sec is not None:
+                    df['timestamp'] = df['timestamp'].dt.floor('{}S'.format(interval_sec))
+                    df = pd.concat([
+                        df.groupby('timestamp')['price'].nth(0).rename('op'),
+                        df.groupby('timestamp')['price'].max().rename('hi'),
+                        df.groupby('timestamp')['price'].min().rename('lo'),
+                        df.groupby('timestamp')['price'].nth(-1).rename('cl'),
+                        df.groupby('timestamp')['size'].sum().rename('volume'),
+                    ], axis=1)
+
                 dfs.append(df)
-            except urllib.error.HTTPError:
-                pass
+
             date += datetime.timedelta(days=1)
 
-        df = pd.concat(dfs).reset_index(drop=True)
+        df = pd.concat(dfs)
+
+        if interval_sec is None:
+            df.reset_index(drop=True, inplace=True)
 
         return df
+
+    def _find_start_year_month(self, market):
+        today = datetime.datetime.now().date()
+
+        for year in range(2018, today.year + 1):
+            url = 'https://api.coin.z.com/data/trades/{}/{}/'.format(market, year)
+            self.logger.debug(url)
+            if self._url_exists(url):
+                start_year = year
+                break
+
+        for month in range(1, 13):
+            url = 'https://api.coin.z.com/data/trades/{}/{}/{:02}/'.format(market, start_year, month)
+            self.logger.debug(url)
+            if self._url_exists(url):
+                start_month = month
+                break
+
+        return start_year, start_month
+
 
 
 
--- tests/test_gmo.py
+++ tests/test_gmo.py
@@ -1,12 +1,30 @@
-import time
-import ccxt
 import pandas as pd
 import logging
 import sys
 from unittest import TestCase, mock
 from crypto_data_fetcher.gmo import GmoFetcher
+from joblib import Memory
+
+memory_location = '/tmp/crypto_data_fetcher_test_gmo'
 
 class TestGmo(TestCase):
+    def test_fetch_ohlcv(self):
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+
+        stderr_handler = logging.StreamHandler(stream=sys.stderr)
+        stderr_handler.setLevel(logging.DEBUG)
+        logger.addHandler(stderr_handler)
+
+        memory = Memory(memory_location, verbose=1)
+        fetcher = GmoFetcher(logger=logger, memory=memory)
+
+        df = fetcher.fetch_ohlcv(market='XLM', interval_sec=300)
+        print(df)
+        print(df.dtypes)
+
+        self.assertEqual(df.index[0], pd.to_datetime('2021-08-18 07:10:00+00:00', utc=True))
+
     def test_fetch_trades(self):
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
@@ -15,9 +33,10 @@ class TestGmo(TestCase):
         stderr_handler.setLevel(logging.DEBUG)
         logger.addHandler(stderr_handler)
 
-        fetcher = GmoFetcher(logger=logger)
+        memory = Memory(memory_location, verbose=1)
+        fetcher = GmoFetcher(logger=logger, memory=memory)
 
-        df = fetcher.fetch_trades('XLM')
+        df = fetcher.fetch_trades(market='XLM')
         print(df)
         print(df.dtypes)
 
